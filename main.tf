e2d73dbf809155a436943fdfeb793fe8f4473419
MUGESH RAJ <154227481+Mugesh2024rj@users.noreply.github.com>
2026-02-03 13:17:28 +0530

    Refactor EC2 instance to use existing security group
    Removed default security group and updated EC2 instance to use an existing security group.

--- main.tf
+++ main.tf
@@ -1,51 +1,14 @@
-# Provider
 provider "aws" {
   region = "us-east-1"
 }
 
-
-# Security Group (default VPC)
-resource "aws_security_group" "ec2_sg" {
-  name        = "Example-Ec2-auto"
-  description = "Allow SSH, HTTP, HTTPS"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTPS"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# EC2 Instance
-  resource "aws_instance" "example" {
+resource "aws_instance" "example" {
   ami           = "ami-04514d500a74cdad8"
   instance_type = "t3.micro"
   key_name      = "key-pair-virginia"
+
+  # USE EXISTING SG HERE
+  vpc_security_group_ids = "sg-0d0dcf48f8c348f4d"
 
   tags = {
     Name = "Ec2-server-auto"
