c4eae11e600df1fd4ac4a1eeb5d673c8fc2cd452
MUGESH RAJ <154227481+Mugesh2024rj@users.noreply.github.com>
2026-02-03 13:19:52 +0530

    Change security group ID to a list format

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "example" {
   key_name      = "key-pair-virginia"
 
   # USE EXISTING SG HERE
-  vpc_security_group_ids = "sg-0d0dcf48f8c348f4d"
+  vpc_security_group_ids = ["sg-0d0dcf48f8c348f4d"]
 
   tags = {
     Name = "Ec2-server-auto"
